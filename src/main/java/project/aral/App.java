6e83a2f9eedcf057d39ac5ded64b2c6213594d36
Turdıbek <turdibekjumabaev05@gmail.com>
2025-11-02 17:06:35 +0500

    test: lexerdi tekseriw

--- src/main/java/project/aral/App.java
+++ src/main/java/project/aral/App.java
@@ -1,13 +1,21 @@
 package project.aral;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import project.aral.lexer.Lexer;
+import project.aral.lexer.Token;
+
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        String code = """
+                ózgeriwshi san a = 5;
+                ózgeriwshi san b = 3;
+                """;
+        Lexer lexer = new Lexer(code);
+        List<Token> tokens = lexer.tokenize();
+
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
     }
 }
